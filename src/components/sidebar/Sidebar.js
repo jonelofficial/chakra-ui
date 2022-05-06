5be099634297a2e1f49c65e63ab1201c4654b325
Jonel <it.jonelfignacio@gmail.com>
2022-05-06 16:51:22 +0800

    Unfinish sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,70 +1,80 @@
 import {
+  Avatar,
   Box,
-  Button,
-  Circle,
   Flex,
   Icon,
+  Input,
   Text,
+  Tooltip,
   useColorMode,
-  VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { navlink } from "../../navlink";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 import "../../styles/style.css";
+import Hamburger from "hamburger-react";
+import { SearchIcon, SettingsIcon } from "@chakra-ui/icons";
 
-const Sidebar = ({ isOpen }) => {
+const Sidebar = ({ isOpen, setOpen }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
+  const [search, setSearch] = useState();
+
   return (
-    <Flex
-      overflowY="auto"
-      overflowX="hidden"
-      flexDirection="column"
-      bg="primary"
-    >
-      <Box borderBottom="1px solid rgba(179,184,212,.2)">
-        <Text
-          color="text"
-          textTransform="uppercase"
-          fontWeight="bold"
-          letterSpacing="1px"
-          overflow="hidden"
-          textOverflow="ellipsis"
-          whiteSpace="nowrap"
-          p="14px"
-        >
-          Logo
-        </Text>
+    <Box bg="primary" h="100%" w="240px" pos="fixed" top="0" p="6px 14px">
+      <Flex alignItems="center">
+        {/* logo_content */}
+        <Flex alignItems="center" h="50px" w="100%">
+          {/* logo */}
+          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" mr="3" />
+          <Text color="text" fontSize="lg">
+            Logo Name
+          </Text>
+        </Flex>
+        <Hamburger
+          hideOutline={false}
+          rounded
+          size={20}
+          color={"#fff"}
+          toggled={isOpen}
+          toggle={setOpen}
+        />
+      </Flex>
+      <Flex flexDirection="column" mt="20px">
+        <Flex alignItems="center">
+          <SearchIcon boxSize={6} color="#fff" mr="3" />
+          <Input
+            color="text"
+            type="text"
+            name="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </Flex>
+        {navlink.map(({ icon, name }) => (
+          <Flex alignItems="center" pos="relative" h="50px" w="100%">
+            <Icon as={icon} boxSize={8} style={{ color: "#fff" }} mr="3" />
+            <Text color="text">{name}</Text>
+            {/* <Text color="text" className="tooltip">
+              {name}
+            </Text> */}
+          </Flex>
+        ))}
+      </Flex>
+      <Box>
+        <Box>
+          <Box>
+            <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+            <Box>
+              <Text color="text">Jonel</Text>
+              <Text color="text">Web Developer</Text>
+            </Box>
+          </Box>
+          <SettingsIcon boxSize={8} color="text" />
+        </Box>
       </Box>
-      <Box flexGrow="1">
-        <VStack pt="20px">
-          {navlink.map(({ icon }) => (
-            <Circle size="40px" bg="secondary" className="sidebar">
-              <Icon
-                as={icon}
-                boxSize={5}
-                style={{ color: "#fff" }}
-                className="sidebar-icon"
-              />
-            </Circle>
-          ))}
-        </VStack>
-      </Box>
-      <Box borderTop="1px solid rgba(179,184,212,.2)">
-        <VStack p="14px">
-          <Button
-            size="sm"
-            onClick={toggleColorMode}
-            borderRadius="full"
-            h="37px"
-          >
-            {colorMode === "light" ? <BsFillSunFill /> : <BsFillMoonFill />}
-          </Button>
-        </VStack>
-      </Box>
-    </Flex>
+    </Box>
   );
 };
 
